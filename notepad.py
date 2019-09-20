957e8eb866baedf2b90cfffd2f6ea6b108d3dbc0
krishna <krishmi139@gmail.com>
2019-09-20 20:49:15 +0530

    New Open Close working

--- notepad.py
+++ notepad.py
@@ -2,26 +2,38 @@ from tkinter import *
 from tkinter import ttk,filedialog
 
 def newFile():
-	pass
+	tab = Text(notebook)
+	notebook.add(tab, text='Untitled'+str(notebook.index('end')))
+	notebook.select(notebook.index('end')-1)
 
 def openFile():
 	filename = filedialog.askopenfilename(initialdir='home/', title="Select file..")
 	tab = Text(notebook)
-	notebook.add(tab1, text=filename)
+	notebook.add(tab, text=filename)
 	with open(filename) as f:
 		text = f.read()
 		tab.insert(INSERT, text)
+		notebook.select(notebook.index('end')-1)
 
 def closeFile():
-	pass
+	if notebook.index(notebook.select()) != 0:
+		notebook.forget(notebook.index(notebook.select()))
+	else:
+		tab = Text(notebook)
+		notebook.add(tab, text='Untitled')
+		notebook.forget(0)
 
 #root
 root = Tk()
 root.title('Notepad')
+root.rowconfigure(0, weight=1)
+root.columnconfigure(0, weight=1)
+
+#main
 mainframe = ttk.Frame(root, height=500, width=500)
 mainframe.grid(row=0, column=0, sticky="nwes")
-root.columnconfigure(1, weight=1)
-root.rowconfigure(1, weight=1)
+mainframe.rowconfigure(0, weight=1)
+mainframe.columnconfigure(0, weight=1)
 
 #Menu
 root.option_add('*tearOff', FALSE)
@@ -35,12 +47,14 @@ menu_file.add_command(label='New', command=newFile)
 menu_file.add_command(label='Open...', command=openFile)
 menu_file.add_command(label='Close', command=closeFile)
 
+root.config(menu=menubar)
+
 #Text file tabs
 notebook = ttk.Notebook(mainframe)
-tab1 = Text(notebook)
-notebook.add(tab1, text='Untitled1')
-notebook.grid()
+tab = Text(notebook)
+notebook.add(tab, text='Untitled')
+notebook.grid(sticky="nwes")
+
 
 #root configuration
-root.config(menu=menubar)
 root.mainloop()
