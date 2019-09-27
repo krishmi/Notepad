2fe9f3737ab8791911d0fea54b36c0c3a81e2ade
krishna <krishmi139@gmail.com>
2019-09-27 11:09:57 +0530

    Basics done

--- notepad.py
+++ notepad.py
@@ -15,6 +15,11 @@ def openFile():
 		tab.insert(INSERT, text)
 		notebook.select(notebook.index('end')-1)
 
+	scrollbar = ttk.Scrollbar(tab, orient=VERTICAL, command=tab.yview)
+	tab['yscrollcommand'] = scrollbar.set
+	scrollbar.pack(side=RIGHT, fill=Y)
+		
+
 def closeFile():
 	if notebook.index(notebook.select()) != 0:
 		notebook.forget(notebook.index(notebook.select()))
@@ -23,17 +28,34 @@ def closeFile():
 		notebook.add(tab, text='Untitled')
 		notebook.forget(0)
 
+def savefile():
+	filename = filedialog.asksaveasfilename()
+	if filename != '':
+		with open(filename, 'w+') as f:
+			hieararchy = notebook.select().split('.')
+			tab = notebook.children[hieararchy[len(hieararchy) - 1]]
+			f.write(tab.get('1.0', 'end'))
+
+def keydown(event):
+	c = event.keysym
+	s = event.state
+	ctrl  = (s & 0x4) != 0
+	if ctrl and (c == 's' or c == 'S'):
+		savefile()
+
 #root
 root = Tk()
 root.title('Notepad')
 root.rowconfigure(0, weight=1)
 root.columnconfigure(0, weight=1)
+root.bind("<KeyPress>", keydown)
 
 #main
 mainframe = ttk.Frame(root, height=500, width=500)
 mainframe.grid(row=0, column=0, sticky="nwes")
 mainframe.rowconfigure(0, weight=1)
 mainframe.columnconfigure(0, weight=1)
+
 
 #Menu
 root.option_add('*tearOff', FALSE)
@@ -45,6 +67,7 @@ menubar.add_cascade(menu=menu_edit, label='Edit')
 
 menu_file.add_command(label='New', command=newFile)
 menu_file.add_command(label='Open...', command=openFile)
+menu_file.add_command(label='Save As', command=savefile)
 menu_file.add_command(label='Close', command=closeFile)
 
 root.config(menu=menubar)
